07c7c1ca6682b835db7e604662dfcad01e32ab62
smuts101 <69514780+smuts101@users.noreply.github.com>
2020-09-30 17:39:40 +0200

    create a form inside a table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AdminBookingComponent } from './admin-booking/admin-booking.component';
+import { AdminUploadComponent } from './admin-upload/admin-upload.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AdminBookingComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminUploadComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
