6248711ee3f4854224f1eb62f51dcad14049be16
Mashiars <69487917+Mashiars@users.noreply.github.com>
2020-09-30 13:34:44 +0200

    admin upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AdminUploadComponent } from './admin-upload/admin-upload.component';
+import { AdminBookingComponent } from './admin-booking/admin-booking.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AdminUploadComponent ],
+  declarations: [ AppComponent, HelloComponent, AdminBookingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
